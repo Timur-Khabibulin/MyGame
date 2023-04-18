ec24cba0e8494c77ea0575bf4c164d7bde695af2
Timur-Khabibulin <101925716+Timur-Khabibulin@users.noreply.github.com>
2023-04-18 22:46:17 +0500

    Добавил интерфейс для существа

---
+++ MyGame/Code/Model/ICreature.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace MyGame.Model;
+
+public interface ICreature
+{
+    public string Name { get; }
+    public Point Coordinates { get; }
+    public int Health { get; }
+
+    public int DamagePower { get; }
+
+    public void Act();
+    public void MakeDamage(ICreature conflictedObject);
+    public void TakeDamage(int damage);
+}
