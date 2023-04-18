96f13ae8624bde52d60d24e51c801d24bec40761
Timur-Khabibulin <101925716+Timur-Khabibulin@users.noreply.github.com>
2023-04-18 22:50:10 +0500

    Добавил игрока и охотника

---
+++ MyGame/Code/Model/Hunter.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace MyGame.Model;
+
+public class Hunter : ICreature
+{
+    public string Name => "Hunter";
+    public int DamagePower => 10;
+    public Point Coordinates { get; }
+    public int Health { get; private set; }
+
+
+    public Hunter(Point coordinates)
+    {
+        Coordinates = coordinates;
+        Health = 100;
+    }
+
+    public void Act()
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    public void MakeDamage(ICreature conflictedObject)
+    {
+        conflictedObject.TakeDamage(DamagePower);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        Health -= damage <= Health ? damage : Health;
+    }
+}
---
+++ MyGame/Code/Model/Player.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace MyGame.Model;
+
+public class Player : ICreature
+{
+    public string Name => "Goose";
+    public int DamagePower => 10;
+    public Point Coordinates { get; }
+    public int Health { get; private set; }
+
+
+    public Player(Point coordinates)
+    {
+        Coordinates = coordinates;
+        Health = 100;
+    }
+
+    public void Act()
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    public void MakeDamage(ICreature conflictedObject)
+    {
+        conflictedObject.TakeDamage(DamagePower);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        Health -= damage <= Health ? damage : Health;
+    }
+}
