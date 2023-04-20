f03d569378caad0fa46025a5ae168aa0a3278fb7
Timur-Khabibulin <101925716+Timur-Khabibulin@users.noreply.github.com>
2023-04-20 20:13:45 +0500

    Добавил пейзаж

---
+++ MyGame/Code/View/World.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MyGame;
+
+public class World : IComponent
+{
+    private Texture2D ground;
+    private int width;
+    private int height;
+    private int groundHeight;
+
+    public World(ContentManager content, int width, int height)
+    {
+        ground = content.Load<Texture2D>("Ground");
+        this.width = width;
+        this.height = height;
+        groundHeight = height / 4;
+    }
+
+    public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+    {
+        spriteBatch.Draw(ground, new Rectangle(0,
+            height - groundHeight,
+            width,
+            groundHeight), Color.White);
+    }
+
+    public void Update(GameTime gameTime)
+    {
+    }
+}
--- MyGame/Game1.cs
+++ MyGame/Game1.cs
@@ -11,10 +11,14 @@ public class Game1 : Game
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
     private SplashScreen _splashScreen;
+    private World _world;
 
     public Game1()
     {
         _graphics = new GraphicsDeviceManager(this);
+        _graphics.PreferredBackBufferWidth = 1920;
+        _graphics.PreferredBackBufferHeight = 1080;
+        _graphics.ToggleFullScreen();
         Content.RootDirectory = "Content";
         IsMouseVisible = true;
     }
@@ -30,6 +34,9 @@ public class Game1 : Game
         _spriteBatch = new SpriteBatch(GraphicsDevice);
 
         _splashScreen = new SplashScreen(Content);
+        _world = new World(Content,
+            _graphics.PreferredBackBufferWidth,
+            _graphics.PreferredBackBufferHeight);
     }
 
     protected override void Update(GameTime gameTime)
@@ -50,6 +57,7 @@ public class Game1 : Game
         _spriteBatch.Begin();
 
         _splashScreen.Draw(gameTime, _spriteBatch);
+        _world.Draw(gameTime, _spriteBatch);
 
         _spriteBatch.End();
 
