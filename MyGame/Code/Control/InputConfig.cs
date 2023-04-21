27a797803e43b9ef9eb9b926857cddc6a1781b8f
Timur-Khabibulin <101925716+Timur-Khabibulin@users.noreply.github.com>
2023-04-21 19:07:08 +0500

    Добавил класс для настройки клавиш управления

---
+++ MyGame/Code/Control/InputConfig.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Xna.Framework.Input;
+
+namespace MyGame.Code.Control;
+
+public static class InputConfig
+{
+    public static Keys Up => Keys.W;
+    public static Keys Down => Keys.S;
+    public static Keys Right => Keys.D;
+    public static Keys Left => Keys.A;
+}
