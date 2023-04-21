3f4bddc08911b3bdc004df302a5d91ae3450c98f
Timur-Khabibulin <101925716+Timur-Khabibulin@users.noreply.github.com>
2023-04-21 19:13:23 +0500

    Добавил интерфейс для управления игровыми сущностями

---
+++ MyGame/Code/Control/IControl.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MyGame.Model;
+
+namespace MyGame.Code.Control;
+
+public interface IControl
+{
+    public void Draw(GameTime gameTime, SpriteBatch spriteBatch);
+    public void Act(KeyboardState keyboardState);
+}
