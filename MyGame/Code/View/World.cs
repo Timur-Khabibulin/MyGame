740540125eda2e1fe19a59429ea90068489901cf
Timur-Khabibulin <101925716+Timur-Khabibulin@users.noreply.github.com>
2023-04-28 22:02:00 +0500

    Добавил стрельбу

--- MyGame/Code/View/World.cs
+++ MyGame/Code/View/World.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
@@ -14,7 +15,8 @@ public class World : IComponent
     private int height;
     private int groundHeight;
 
-    private Player player;
+    private List<IControl> entities;
+    private List<Bullet> bullets;
 
     public World(ContentManager content, int width, int height)
     {
@@ -22,7 +24,11 @@ public class World : IComponent
         this.width = width;
         this.height = height;
         groundHeight = height / 4;
-        player = new Player(content, width, height);
+        entities = new List<IControl>();
+        bullets = new List<Bullet>();
+
+        entities.Add(new Player(content, bullets, width, height));
+        entities.Add(new Hunter(content, bullets, entities[0] as Player));
     }
 
     public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
@@ -32,12 +38,22 @@ public class World : IComponent
             width,
             groundHeight), Color.White);
 
-        player.Draw(gameTime, spriteBatch);
+        foreach (var entity in entities)
+            entity.Draw(gameTime, spriteBatch);
+
+        foreach (var bullet in bullets)
+            bullet.Draw(spriteBatch);
     }
 
     public void Update(GameTime gameTime)
     {
         var keyboardState = Keyboard.GetState();
-        player.Act(keyboardState);
+        var mouseState = Mouse.GetState();
+
+        foreach (var entity in entities)
+            entity.Update(gameTime, keyboardState, mouseState);
+
+        foreach (var bullet in bullets)
+            bullet.Update();
     }
 }
