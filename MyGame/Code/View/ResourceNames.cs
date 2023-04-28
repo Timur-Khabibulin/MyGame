2fec84df75eda1954d6164f21276467a06513433
Timur-Khabibulin <101925716+Timur-Khabibulin@users.noreply.github.com>
2023-04-28 21:58:55 +0500

    Добавил текстуры

--- MyGame/Code/View/ResourceNames.cs
+++ MyGame/Code/View/ResourceNames.cs
@@ -1,10 +1,14 @@
-﻿namespace MyGame;
+﻿using MyGame.Code.Control;
+
+namespace MyGame;
 
 public static class ResourceNames
 {
     public static string Button => "Button";
     public static string Goose => "Goose";
     public static string Ground => "Ground";
+    public static string Bullet => "Bullet";
+    public static string Hunter => "Hunter";
 
     public static string ButtonFont => "ButtonFont";
 }
