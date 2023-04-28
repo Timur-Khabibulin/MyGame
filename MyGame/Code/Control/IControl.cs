0d621275e1a5e85ab4dc140bcd15b7a01df43094
Timur-Khabibulin <101925716+Timur-Khabibulin@users.noreply.github.com>
2023-04-28 21:59:53 +0500

    Поменял интерфейс

--- MyGame/Code/Control/IControl.cs
+++ MyGame/Code/Control/IControl.cs
@@ -1,11 +1,15 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using MyGame.Model;
 
 namespace MyGame.Code.Control;
 
 public interface IControl
 {
+    public ICreature Entity { get; }
+    public Rectangle Rectangle { get; }
+
     public void Draw(GameTime gameTime, SpriteBatch spriteBatch);
-    public void Act(KeyboardState keyboardState);
+    public void Update(GameTime gameTime, KeyboardState keyboardState, MouseState mouseState);
 }
