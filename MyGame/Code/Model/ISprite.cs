a0a3c5c845cb6d8dabd2c5d273c66fe978616627
Timur-Khabibulin <101925716+Timur-Khabibulin@users.noreply.github.com>
2023-05-03 22:55:26 +0500

    Добавил интерфейс для спрайта

---
+++ MyGame/Code/Model/ISprite.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MyGame.Code.Model;
+
+public interface ISprite
+{
+    public Texture2D Texture { get; }
+    public Rectangle ViewArea { get; }
+    public Vector2 Position { get;}
+
+    public Vector2 Min { get; }
+
+    public Vector2 Max { get; }
+
+    public void Draw(SpriteBatch spriteBatch);
+
+    public void Update(GameTime gameTime);
+}
