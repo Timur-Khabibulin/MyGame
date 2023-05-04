f436e16dba4dc5c7aa0be0e1664e81577330e407
Timur-Khabibulin <101925716+Timur-Khabibulin@users.noreply.github.com>
2023-05-04 23:01:55 +0500

    Реализовал спавн врагов

---
+++ MyGame/Code/Model/CreaturesManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using MyGame.Code.Model.Entities;
+
+namespace MyGame.Code.Model;
+
+public class CreaturesManager
+{
+    public IReadOnlyCollection<BaseCreature> Creatures => creatures;
+
+    private const int AppearancePeriod = 2;
+    private readonly Vector2 hunterStartPosition = new(1600, 720);
+
+    private readonly ContentManager contentManager;
+    private readonly List<BaseCreature> creatures;
+    private readonly Goose player;
+    private double timer;
+    private int width;
+    private int height;
+
+    public CreaturesManager(ContentManager contentManager, BaseCreature player, int width, int height)
+    {
+        this.contentManager = contentManager;
+        creatures = new List<BaseCreature>() { player };
+        this.player = (Goose)player;
+        this.width = width;
+        this.height = height;
+    }
+
+    public void Update(GameTime gameTime)
+    {
+        if (gameTime.TotalGameTime.TotalSeconds - timer > AppearancePeriod)
+        {
+            creatures.Add(new Hunter(player, hunterStartPosition,
+                contentManager,
+                new Vector2(0, 0),
+                new Vector2(width, height)));
+            timer = gameTime.TotalGameTime.TotalSeconds;
+        }
+    }
+}
